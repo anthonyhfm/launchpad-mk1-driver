5c032dd20800d4779c20b9daa79a660d2d2c47a8
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2026-07-11 10:35:39 +0100

    Initial Commit

---
+++ NOVLPD01-MAC/NOVLPD01_MACApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOVLPD01_MACApp.swift
+//  NOVLPD01-MAC
+//
+//  Created by Anthony Hofmeister on 11.07.26.
+//
+
+import SwiftUI
+
+@main
+struct NOVLPD01_MACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
